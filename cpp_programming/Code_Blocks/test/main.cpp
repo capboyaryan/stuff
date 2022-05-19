1b0c437879a516363cc682015fa483c576f3af03
capboyaryan <capboyaryan@gmail.com>
2022-05-19 21:01:33 +0530

    STUFF

--- cpp_programming/Code_Blocks/test/main.cpp
+++ cpp_programming/Code_Blocks/test/main.cpp
@@ -1,25 +1,57 @@
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//    fstream f;
+//    f.open("test.txt",ios::in);
+//    string line;
+//    int tell;
+//
+//    while(f)
+//    {
+//        getline(f,line);
+//        cout<<line<<"\n";
+//        if(f.tellg()!=(-1))
+//        {
+//            cout<<"\nLOGIC\n";
+//        }
+//        tell=f.tellg();
+//        //cout<<tell<<"\n";
+//    }
+//
+//    return 0;
+//}
+#include <sstream>
+#include <vector>
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
 
-int main()
-{
-    fstream f;
-    f.open("test.txt",ios::in);
-    string line;
-    int tell;
-
-    while(f)
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> temp;
+    stringstream stream(str);
+    int temp_num=0,temp_char;
+    for(;stream;)
     {
-        getline(f,line);
-        cout<<line<<"\n";
-        if(f.tellg()!=(-1))
-        {
-            cout<<"\nLOGIC\n";
-        }
-        tell=f.tellg();
-        //cout<<tell<<"\n";
+        stream>>temp_num;
+        temp.push_back(temp_num);
+        if(!stream)
+            break;
+        stream>>temp_char;
+    }
+    return temp;
+
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
     }
 
     return 0;
---
+++ sort.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <windows.h>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void display(const vector<int> &v)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<i<<'|'<<setw(v.at(i)+1)<<setfill('-')<<v.at(i)<<'\n';
+    }
+    cout<<" |__________\n";
+}
+
+void display(const vector<int> &v,int color_code,int pos)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        if(i==pos)
+            SetConsoleTextAttribute(hConsole,color_code);
+        cout<<i<<'|'<<setw(v.at(i)+1)<<setfill('-')<<v.at(i)<<'\n';
+        if(i==pos)
+            SetConsoleTextAttribute(hConsole,15);
+    }
+    cout<<" |__________\n";
+}
+
+void display(const vector<int> &v,int color_code_1,int pos_1,int color_code_2,int pos_2)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        if(i==pos_1)
+            SetConsoleTextAttribute(hConsole,color_code_1);
+        if(i==pos_2)
+            SetConsoleTextAttribute(hConsole,color_code_2);
+        cout<<i<<'|'<<setw(v.at(i)+1)<<setfill('-')<<v.at(i)<<'\n';
+        if(i==pos_1 || i==pos_2)
+            SetConsoleTextAttribute(hConsole,15);
+    }
+    cout<<" |__________\n";
+}
+
+
+int main()
+{
+    vector<int> vec={2,4,3,1,5};
+    int checker=0,l=0;
+
+    system("cls");
+    display(vec,15,-1);
+    this_thread::sleep_for(std::chrono::seconds {2});
+    
+    for(;;)
+    {
+        for(l=0,checker=0;l<(vec.size()-1);l++)
+        {
+            system("cls");
+            display(vec,9,l,9,l+1);
+            this_thread::sleep_for(std::chrono::seconds {2});
+            
+            if(vec.at(l)>vec.at(l+1))
+            {
+                system("cls");
+                display(vec,12,l,12,l+1);
+                this_thread::sleep_for(std::chrono::seconds {2});
+
+                int temp=vec.at(l);
+                vec.at(l)=vec.at(l+1);
+                vec.at(l+1)=temp;
+
+                system("cls");
+                display(vec,12,l,12,l+1);
+                this_thread::sleep_for(std::chrono::seconds {2});
+            }
+            else 
+                checker++;
+
+            system("cls");
+            display(vec);
+        }
+        if(checker==vec.size()-1)
+            break;
+    }
+}
